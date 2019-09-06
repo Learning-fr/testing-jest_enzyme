e70d92ab4b6b4908040ef79f8a44bf365b0dbe71
frivolta <rivoltafilippo@gmail.com>
2019-09-06 22:33:37 +0200

    :white_check_mark: Adding app test

--- testing-simple-app/src/App.js
+++ testing-simple-app/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    }
+  }
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">Current counter is: {this.state.counter}</h1>
+        <button
+          data-test="increment-button"
+          onClick={() => { this.setState({ counter: this.state.counter + 1 }) }}
+        >
+          Increment counter
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- testing-simple-app/src/App.test.js
+++ testing-simple-app/src/App.test.js
@@ -6,6 +6,64 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  shallow(<App />)
+
+/**
+ * Factory function to create shallow wrapper for te App component
+ * @param {object} props - App props specific for this setup
+ * @param {object} state - Initial state
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />)
+  if (state) {
+    wrapper.setState(state)
+  }
+  return wrapper
+}
+
+/**
+ * Returns ShallowWrapper containing node(s) with data-test attribute
+ * @param {ShallowWrapper} wrapper - Shallow wrapper given by setup
+ * @param {string} dataName  - Data test name attribute
+ * @returns {ShallowWrapper}
+ */
+const findByDataAttr = (wrapper, dataName) => {
+  return wrapper.find(`[data-test="${dataName}"]`)
+}
+
+test('renders without errors', () => {
+  const wrapper = setup()
+  const appComponent = findByDataAttr(wrapper, 'component-app')
+  expect(appComponent.length).toBe(1);
 });
+
+test('renders increment button', () => {
+  const wrapper = setup();
+  const incrementButton = findByDataAttr(wrapper, 'increment-button')
+  expect(incrementButton.length).toBe(1);
+})
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByDataAttr(wrapper, 'counter-display')
+  expect(counterDisplay.length).toBe(1);
+})
+
+test('counters start at 0', () => {
+  const wrapper = setup()
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0)
+})
+
+test('clicking button increment counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  //Simulate button click
+  const button = findByDataAttr(wrapper, 'increment-button');
+  button.simulate('click');
+  wrapper.update();
+  //Test if counterdisplay contains counter + 1
+  const counterDisplay = findByDataAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1);
+})
