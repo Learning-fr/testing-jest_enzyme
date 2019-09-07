3efb2660239c29b11e042655136fe76fd1b17a51
frivolta <rivoltafilippo@gmail.com>
2019-09-07 22:21:42 +0200

    :white_check_mark: Adding GuessedWords zero properties tests

--- testing-jotto-redux-thunk-axios/src/Congrats/Congrats.test.js
+++ testing-jotto-redux-thunk-axios/src/Congrats/Congrats.test.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-
+import { shallow } from 'enzyme';
 import Congrats from './Congrats';
 import { findByTestAttr } from '../../test/testUtils';
 import { checkProps } from '../../test/testUtils'
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { succes: false }
 
